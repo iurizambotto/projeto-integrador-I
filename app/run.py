6d3f52b7509f2085de7a5bdc50d16f2cf17085a3
Iuri Zambotto <iuri.zambotto@kovi.com.br>
2021-11-23 17:03:20 -0300

    test run file different

--- app/run.py
+++ app/run.py
@@ -1,6 +1,7 @@
-from app import app as application
-
-if __name__ == '__main__':
-    application.run()
+from app import app
 
 # app = application.run()
+
+if __name__ == '__main__':
+    app.run()
+
